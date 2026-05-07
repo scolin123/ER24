011a069b6021e5f197dcb5c1b23eaa38b1985300
Colin Shier <colin@shier.ca>
2026-05-07 18:17:00 -0400

    load all games from data directory

--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <dirent.h>
+
 #include "baseball.h"
 
 int main(void){
-    struct pitch pitches[500];
+    struct pitch pitches[20000];
 
-    int count = parse_csv("data/2025 Guelph Royals Analytics - May 11 - Kitchener vs. Toronto.csv", pitches, 500);
-    printf("%s\n",pitches[21].batter);
-    printf("%d\n",pitches[21].runners);
 
-    struct plateAppearance pas[200];
-    build_plate_appearance(pitches,count,pas,200);
+    DIR *d = opendir("data/");
+    if(d == NULL){
+        return -1;
+    }
+    int pitch_count = 0;
+    struct dirent *entry;
+    while((entry = readdir(d)) != NULL){
+        if(entry->d_name[0] =='.'){
+            continue;
+        }
+        char filepath[512];
+        snprintf(filepath,sizeof(filepath),"data/%s",entry->d_name);
+        int n = parse_csv(filepath,pitches + pitch_count, 20000 - pitch_count);
+        if(n > 0){
+            pitch_count += n;
+        }
+    }
+    closedir(d);
+    printf("Total pitches: %d\n",pitch_count);
+
+    struct plateAppearance pas[2000];
+    build_plate_appearance(pitches,pitch_count,pas,2000);
 
     for(int i = 0; i < 5; i++){
         printf("%s\n",pas[i].batter);
