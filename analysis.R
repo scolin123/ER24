4c5a8e620dbcc1ee411ccd1396143000c88c99a2
Colin Shier <colin@shier.ca>
2026-05-12 16:58:52 -0400

    Individual platyer matrix in R

--- analysis.R
+++ analysis.R
@@ -1,3 +1,8 @@
 df <- read.csv("outputs/re24.csv")
 df$avg_runs <- ifelse(df$pa_count > 0, df$total_runs / df$pa_count, NA)
 print(df)
+
+pas <- read.csv("outputs/pas.csv");
+player_re24 <- aggregate(runs_scored ~ batter, data = pas, sum)
+player_re24 <-  player_re24[order(-player_re24$runs_scored), ]
+print(player_re24)
